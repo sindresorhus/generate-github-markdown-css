37f6201c96904a33b0614cc8a6d48def06c5d5f8
Sam Verschueren <sam.verschueren@gmail.com>
2016-05-02 17:59:25 +0200

    promisify and lipstickify

--- test.js
+++ test.js
@@ -1,13 +1,6 @@
-'use strict';
-var assert = require('assert');
-var githubMarkdownCss = require('./');
+import test from 'ava';
+import m from './';
 
-it('should get the GitHub Markdown CSS', function (cb) {
-	this.timeout(500000);
-
-	githubMarkdownCss(function (err, css) {
-		assert(!err, err);
-		assert(/markdown-body/.test(css));
-		cb();
-	});
+test(async t => {
+	t.regex(await m(), /markdown-body/);
 });
