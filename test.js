02992c49c1463d6e40bc2cae8d4de302a12a2367
Clement GASTON <clement.gaston@viacesi.fr>
2022-07-14 21:52:08 +0200

    Support all GitHub themes

--- test.js
+++ test.js
@@ -5,9 +5,24 @@ import githubMarkdownCss from './index.js';
 (async () => {
 	fs.mkdirSync('dist', {recursive: true});
 
-	fs.writeFileSync('dist/auto.css', await githubMarkdownCss());
-	fs.writeFileSync('dist/light.css', await githubMarkdownCss({dark: 'light'}));
-	fs.writeFileSync('dist/dark.css', await githubMarkdownCss({light: 'dark'}));
+	const themes = (await githubMarkdownCss({list: true})).split('\n');
+
+	fs.writeFileSync('dist/all.css', await githubMarkdownCss());
+
+	const promises = [];
+
+	for (const theme of themes) {
+		promises.push(new Promise((resolve, reject) => {
+			githubMarkdownCss({themes: [theme]})
+				.then(css => {
+					fs.writeFileSync(`dist/${theme}.css`, css);
+					resolve();
+				})
+				.catch(reject);
+		}));
+	}
+
+	Promise.all(promises);
 
 	const fixture = await renderMarkdown();
 	const html = `<!DOCTYPE html>
@@ -16,16 +31,15 @@ import githubMarkdownCss from './index.js';
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
 <title>GitHub Markdown CSS demo</title>
-<link id="theme" rel="stylesheet" href="auto.css">
+<link id="theme" rel="stylesheet" href="all.css">
 <body class="markdown-body">
 <article class="markdown-body" style="padding: 1em; max-width: 42em; margin: 0px auto;">
 ${fixture}
 </article>
 <select style="position: fixed; top: 1em; right: 1em; font-size: 16px;"
 				onchange="theme.href=this.value">
-	<option value="auto.css">auto</option>
-	<option value="light.css">light</option>
-	<option value="dark.css">dark</option>
+	<option value="all.css">all</option>
+	${themes.map(theme => `<option value="${theme}.css">${theme}</option>`).join('\n')}
 </select>
 </body>
 </html>
